bc6565c981edb9d683108b5d8144c8c2b4a3b993
Shohei Shibata <shohei.shibata.dev@gmail.com>
2020-04-02 05:17:05 -0400

    mobile nav menu toggle by javascript

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,17 @@
 function toggleNav() {
-	console.log("toggle nav");
 	var toggle = document.getElementById("nav-menu-mobile-toggle");
+	var list = document.getElementById("nav-menu-list");
+	var hamburger = document.getElementById("nav-icon-mobile-open");
+	var x = document.getElementById("nav-icon-mobile-close");
 	if (toggle.checked) {
 		// Hide menu, show hamburger icon
-		console.log("checked");
+		list.style.display = "none";
+		hamburger.style.display = "inline";
+		x.style.display = "none";
 	} else {
 		// Display menu, show close icon
-		console.log("unchecked");
+		list.style.display = "block";
+		hamburger.style.display = "none";
+		x.style.display = "inline";
 	}
 }

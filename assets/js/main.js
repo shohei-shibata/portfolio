b8b0512957bfa9df94b0329b20295185ac6def06
Shohei Shibata <shohei.shibata.dev@gmail.com>
2020-04-03 06:15:41 -0400

    Angled border using clip-path. Adjust font size for mobile (base on view height)

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,15 +3,21 @@ function toggleNav() {
 	var list = document.getElementById("nav-menu-list");
 	var hamburger = document.getElementById("nav-icon-mobile-open");
 	var x = document.getElementById("nav-icon-mobile-close");
+	var body = document.body;
+
+	window.scrollTo(0,0);
+
 	if (toggle.checked) {
 		// Hide menu, show hamburger icon
 		list.style.display = "none";
 		hamburger.style.display = "inline";
 		x.style.display = "none";
+		body.style.overflowY = "scroll";
 	} else {
 		// Display menu, show close icon
 		list.style.display = "block";
 		hamburger.style.display = "none";
 		x.style.display = "inline";
+		body.style.overflowY = "hidden";
 	}
 }

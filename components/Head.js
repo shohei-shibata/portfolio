fe9fe393ecc57b5df9007f817161b81b07ae7694
Shohei Shibata <shohei.shibata.dev@gmail.com>
2020-04-18 12:39:41 -0400

    wip setting up head

---
+++ components/Head.js
@@ -0,0 +1,10 @@
+import HeadComponent from '../components/Head';
+
+const Index = () => (
+  <>
+    <Header />
+    <h1>Hello</h1>
+  </>
+);
+
+export default Index;
---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import Head from '../components/Head';
+
+const Index = () => (
+  <>
+    <Head />
+    <h1>Hello</h1>
+  </>
+);
+
+export default Index;

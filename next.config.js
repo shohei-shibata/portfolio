fbf00419786231505056ed70502ed79b7daa1ecc
Shohei Shibata <shohei.shibata.dev@gmail.com>
2020-04-21 06:28:24 -0400

    mdx finally working... onto fixing page layout next

--- next.config.js
+++ next.config.js
@@ -1,6 +1,17 @@
-const withCSS = require('@zeit/next-css')
+const withPlugins = require('next-compose-plugins');
+//const withCSS = require('@zeit/next-css');
 const withFonts = require('next-fonts');
-const withMDX = require('@next/mdx');
+const withMDX = require("@next/mdx")({
+    // parse mdx files
+    extension: /\.mdx?$/
+});
 
-//module.exports = withCSS(withFonts(withMdx()));
-module.exports = withMDX();
+const nextConfig = {
+  pageExtensions: ['md', 'mdx', 'jsx', 'js'],
+};
+
+module.exports = withPlugins([
+  withFonts,
+  withMDX
+], nextConfig);
+
